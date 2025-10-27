8f4850ff12bc81a06bc680064f3dd71033a12ea5
nils-fjp <nils.fj.paulsson@proton.me>
2025-10-27 10:48:00 +0100

    add style.css

--- 251027-if/app.js
+++ 251027-if/app.js
@@ -22,3 +22,21 @@ userProfileData = userProfileData || "no data available";
 console.log(userProfileData);
 
 const setting = "Admin" && 0 && "User";
+
+// 5. boolean math & coercion
+
+const boolMath = true + true;
+const boolCheck = boolMath == '2';
+
+// 2 == '2'
+
+console.log(typeof boolMath);
+console.log(boolMath);
+console.log(boolCheck);
+
+// 6. nullCheck
+
+const nullVar = null;
+const nullCheck = nullVar == 0;
+console.log(typeof nullVar);
+console.log(typeof 0);
