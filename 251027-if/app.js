355a4f866e16501932409642a77741f596b3eab9
nils-fjp <nils.fj.paulsson@proton.me>
2025-10-27 09:44:32 +0100

    add index, populate app

---
+++ 251027-if/app.js
@@ -0,0 +1,24 @@
+let userLevel = 0;
+let isAdmin = null;
+let data = {
+    isAdmin: true,
+    isSignedIn: true,
+};
+
+let isSignedIn = null;
+let userProfileData = null;
+
+isAdmin = data.isAdmin;
+isSignedIn = data.isSignedIn;
+
+let showAdminSite = isSignedin && isAdmin;
+
+userLevel = userLevel || 1;
+const canAccess = isAdmin && userLevel > 0;
+
+console.log(canAccess);
+
+userProfileData = userProfileData || "no data available";
+console.log(userProfileData);
+
+const setting = "Admin" && 0 && "User";
